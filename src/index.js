5cff0bc0434bec6c2919057b5fbe57b0f749f545
Innocent Onyemaenu <innocentonyemaenu@Nates-MacBook-Pro.local>
2019-06-25 09:19:37 +0100

    mutation type and schema separation

--- src/index.js
+++ src/index.js
@@ -1,19 +1,37 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-    type Query{
-        info: String!
-    }
-`
 
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+
+let linkLength = links.length
 const resolvers = {
     Query:{
         info: ()=> `This is the hackernews clone`,
+        feed: ()=> links,
+        link: (parent,args)=>{
+            console.log('hello'+args)
+            return links.find(link => link.id === args.id)
+        }
+    },
+    Mutation: {
+        post: (parent, args)=>{
+            const link = {
+                id: `link-${linkLength+1}`,
+                description: args.description,
+                url: args.url
+            }
+            links.push(link)
+            return link
+        }
     }
 }
 
 const servers = new GraphQLServer({
-    typeDefs,
+    typeDefs:'./schema.graphql',
     resolvers
 });
 
