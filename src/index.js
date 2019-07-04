9f2721acac64a142723c50602cf6f77425f03fbf
Akhigbe-E <akhigbemmanuel@gmail.com>
2019-07-04 22:56:36 +0100

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,41 +1,30 @@
-const { GraphQLServer } = require('graphql-yoga');
-const {prisma} = require('./generated/prisma-client/index')
-
+const { GraphQLServer } = require("graphql-yoga");
+const { prisma } = require("./generated/prisma-client/index");
 
 const resolvers = {
-    Query:{
-        info: ()=> `This is the hackernews clone`,
-        feed: (root, args, context, info) => {
-            return context.prisma.links()
-        },
-        link: (root, args, context, info)=>{
-            return context.prisma.link({where:args.id})
-            
-        }
+  Query: {
+    info: () => `This is the hackernews clone`,
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
     },
-    Mutation: {
-        post: (root, args, context) => {
-            return context.prisma.createLink({
-                url: args.url,
-                description: args.description,
-            })
-        },
-        // deleteLink: (parent, args)=>{
-        //     return links.filter(link=>!(link.id===args.id));
-        // },
-        // updateLink:(parent, args)=>{
-        //     targetLink = links.find(link=>link.id===args.id);
-        //     targetLink.url = args.url;
-        //     targetLink.description = args.description;
-        //     return targetLink;
-        // }
+    link: (root, args, context, info) => {
+      return context.prisma.link({ where: args.id });
     }
-}
+  },
+  Mutation: {
+    post: (root, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      });
+    }
+  }
+};
 
 const servers = new GraphQLServer({
-    typeDefs:'./src/schema.graphql',
-    resolvers,
-    context: { prisma },
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+  context: { prisma }
 });
 
-servers.start(()=> console.log(`Hello from the server side...`))
+servers.start(() => console.log(`Hello from the server side...`));
