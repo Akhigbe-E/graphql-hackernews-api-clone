d82f209e4e2eba19463c3e9efafd4011a39e5223
Akhigbe-E <akhigbemmanuel@gmail.com>
2019-07-05 04:36:52 +0100

    Filtering functionality added

--- resolvers/Link.js
+++ resolvers/Link.js
@@ -2,6 +2,7 @@ function postedBy(parent, args, context) {
   return context.prisma.link({ id: parent.id }).postedBy();
 }
 function votes(parent, args, context) {
+  console.log(parent.id);
   return context.prisma.link({ id: parent.id }).votes();
 }
 
