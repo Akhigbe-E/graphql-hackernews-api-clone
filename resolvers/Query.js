76e251fe50ac7e4da93b2d6090a33c7701c0a09c
Akhigbe-E <akhigbemmanuel@gmail.com>
2019-07-09 10:22:37 +0100

    Prisma OR operator syntax fix

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -2,10 +2,8 @@ async function feed(parent, args, context, info) {
   const where = args.filter
     ? {
         OR: [
-          {
-            description_contains: args.filter,
-            url_contains: args.filter
-          }
+          { description_contains: args.filter },
+          { url_contains: args.filter }
         ]
       }
     : {};
